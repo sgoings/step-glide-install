bdc85cb5c86711157ad94cbaae8adccdad849d88
Seth Goings <sgoings@engineyard.com>
2015-12-07 13:21:50 -0700

    upgrade glide to 0.7.2

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-set -e
+set -eo pipefail
 
 debug "$("$WERCKER_STEP_ROOT/glide" --version)"
 info "using GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)"
 
 export GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)
 "$WERCKER_STEP_ROOT/glide" install
+
