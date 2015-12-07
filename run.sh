a39adedadf4bc9edc0406949e709fd9907ca8e92
Seth Goings <sgoings@engineyard.com>
2015-12-07 14:30:58 -0700

    move all logic to run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 set -eo pipefail
 
-debug "$("$WERCKER_STEP_ROOT/glide" --version)"
-info "using GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)"
+export GLIDE_VERSION="0.7.2"
+export GO15VENDOREXPERIMENT=1
 
-export GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)
-"$WERCKER_STEP_ROOT/glide" install
+curl -L -o glide-${GLIDE_VERSION}.tar.gz \
+           https://github.com/Masterminds/glide/releases/download/${GLIDE_VERSION}/glide-${GLIDE_VERSION}-linux-amd64.tar.gz
+tar -zxf glide-${GLIDE_VERSION}.tar.gz
+mv linux-amd64/glide /go/bin
+rm -rf linux-amd64 glide-${GLIDE_VERSION}.tar.gz
 
+debug "$(glide --version)"
+info "using GOPATH=$(glide gopath)"
+
+export GOPATH="$(glide gopath)"
+glide install
